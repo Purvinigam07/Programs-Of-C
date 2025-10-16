028807c86b00c135b88a3d4e35b53b5edcabafb8
PurviNigam <sushilnigam199@gmail.com>
2025-10-16 10:03:37 +0530

    Add files via upload

---
+++ octal-hexadecimal.c
@@ -0,0 +1,11 @@
+//Decimal to octal and hexadecimal
+#include <stdio.h>
+
+int main() {
+    int decimalNum;
+    scanf("%d", &decimalNum);
+
+    printf("%X\n", decimalNum);
+    printf("%o\n", decimalNum);
+return 0;
+}
